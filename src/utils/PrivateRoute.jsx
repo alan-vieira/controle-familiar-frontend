f71757e6768264e0b952e56f34ea4c3f6f4ea59f
alan-vieira <alansilvavieira@gmail.com>
2025-11-29 16:19:27 -0300

    fix: subistituir token por auth_token no PrivateRoute.jsx

--- src/utils/PrivateRoute.jsx
+++ src/utils/PrivateRoute.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 function PrivateRoute({ children }) {
   const navigate = useNavigate();
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('auth_token');
 
   useEffect(() => {
     if (!token) {
