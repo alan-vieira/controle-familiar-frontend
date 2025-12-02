3dbf014295d0c86fd1a3c69174dea3f1878c4013
alan-vieira <alansilvavieira@gmail.com>
2025-12-02 08:24:49 -0300

    fix: corrigir formatCurrency.js, corverter string em decimal

--- src/utils/formatCurrency.js
+++ src/utils/formatCurrency.js
@@ -1,11 +1,6 @@
 // src/utils/formatCurrency.js
 export const formatCurrency = (value) => {
-  const num = typeof value === 'string' ? parseFloat(value) : value;
-  if (typeof num !== 'number' || isNaN(num)) {
-    return 'R$ 0,00';
-  }
-  return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  }).format(num);
+  const num = Number(value);
+  if (isNaN(num)) return 'R$ 0,00';
+  return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(num);
 };
