c46b1ea5f193291745a47c51b978921287f48b17
alan-vieira <alansilvavieira@gmail.com>
2025-12-04 03:53:02 -0300

    fix: corrigindo para o login do google funcionar

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function PrivateRoute({ children }) {
   useEffect(() => {
     const checkSession = async () => {
       try {
-        const {  { session } } = await supabase.auth.getSession();
+        const { data: { session } } = await supabase.auth.getSession();
         if (!session) {
           navigate('/login', { replace: true });
         } else {
