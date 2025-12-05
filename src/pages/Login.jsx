42874e2750a4f9b4ec9ae62bd6bb9276708fa3bc
alan-vieira <alansilvavieira@gmail.com>
2025-12-05 02:26:24 -0300

    fix: resolvendo problema com PKCE

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,11 +12,8 @@ export default function Login() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
-          queryParams: {
-            access_type: 'offline',
-            prompt: 'consent',
-          }
+          redirectTo: `${window.location.origin}/auth/callback`, // ← SEM .html
+          queryParams: { access_type: 'offline', prompt: 'consent' }
         }
       });
 
