ea1680a7d4a656609fdfab481e027c8a728a78f3
alan-vieira <alansilvavieira@gmail.com>
2025-12-05 01:29:31 -0300

    fix: correções e testes para flux de login]

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 // src/main.jsx
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(r => r.unregister());
+  });
+}
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css'; // 👈 Importa o Tailwind
